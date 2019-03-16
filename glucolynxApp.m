aec20ef436bf26eaef84dc91cce3525592e72c76
Thein Htut <theinhtut.dec@gmail.com>
2019-03-16 15:06:45 +0800

    Added Pretrained Model

--- glucolynxApp.m
+++ glucolynxApp.m
@@ -6,16 +6,21 @@ clear;
 uiwait(msgbox({'Welcome to Glucolynx v1.0';'Your Blood Glucose Level Monitoring App';'Press OK to choose your file(*.csv)'},'Glucolynx v1.0', 'help'));
 
 % Browse file method
-[filename, pathname]=uigetfile({'*.csv'}, 'File Selector');
+[filename, pathname]=uigetfile({'*.csv'}, 'Select your NIR Blood Glucose Data File');
 fullLocation = strcat(pathname,filename);
 fprintf('%s\n', fullLocation);
 
-% Get file
-importedCSV = importdata(fullLocation);
-%importedCSV.data;
+% Get file and transpose the data
+xTestPerson = xlsread(fullLocation, 'B23:B250');
+xTestPerson = xTestPerson.'; 
+% A=i(21:248,2);
+% importedCSV = importdata(fullLocation);
+% i=importedCSV.data;
+% data = readtable(fullLocation);
 
 
-
+% Load Pre-trained Model
+load('pretrainedModel.mat');
 
 % [YourImage, ~, ImageAlpha] = imread('favicon.png');
 % s=image(YourImage, 'AlphaData', ImageAlpha);
